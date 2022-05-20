9672bf57d6ef718f69dd1cbeed6681650ade13af
Briyan A <briyana1997@gmail.com>
2022-05-20 15:57:10 -0400

    Agregando proyecto al repo

---
+++ api/src/models/Dog.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      altura: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      peso: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      tiempo_vida: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
